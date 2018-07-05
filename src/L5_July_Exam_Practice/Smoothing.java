78c7435d425b32528ff399c77d06ebe251dd264e
Gordon <flemingrgordon@gmail.com>
2018-07-05 21:44:26 +0200

    Laptop

--- src/L5_July_Exam_Practice/Smoothing.java
+++ src/L5_July_Exam_Practice/Smoothing.java
@@ -14,7 +14,7 @@ public class Smoothing {
         int [] pixels = {0, 0, 1, 2, 5, 20, 25, 16, 8, 3,0, 0};
         int [] smooth = new int [12];
         
-            for(int i = 1;i<pixels.length-1;i++){
+            for(int i = 2;i<pixels.length-1;i++){
                 smooth[i] = (pixels[i-1]+pixels[i+1])/2;
                 System.out.print(" {"+smooth[i]+"} ");
         }
